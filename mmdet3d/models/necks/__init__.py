3787406b7a7a914ca39ad946cf855dd1e969c2dd
Danila Rukhovich <danrukh@gmail.com>
2021-07-28 17:01:25 +0300

    try MEFPN3DV2

--- mmdet3d/models/necks/__init__.py
+++ mmdet3d/models/necks/__init__.py
@@ -1,6 +1,6 @@
 from mmdet.models.necks.fpn import FPN
 from .imvoxel_neck import OutdoorImVoxelNeck
 from .second_fpn import SECONDFPN
-from .me_fpn import MEFPN3D
+from .me_fpn import MEFPN3D, MEFPN3DV2
 
 __all__ = ['FPN', 'SECONDFPN', 'OutdoorImVoxelNeck']
--- mmdet3d/models/necks/me_fpn.py
+++ mmdet3d/models/necks/me_fpn.py
@@ -52,3 +52,53 @@ class MEFPN3D(nn.Module):
             ME.MinkowskiBatchNorm(out_channels),
             ME.MinkowskiELU()
         )
+
+
+@NECKS.register_module()
+class MEFPN3DV2(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(MEFPN3DV2, self).__init__()
+        self.n_scales = len(in_channels)
+        for i in range(self.n_scales):
+            if i > 0:
+                self.__setattr__(f'up_block_{i}', self._make_up_block(out_channels))
+            self.__setattr__(f'lateral_block_{i}', self._make_block(in_channels[i], out_channels))
+
+    def forward(self, inputs):
+        outs = []
+        x = self.__getattr__(f'lateral_block_{self.n_scales - 1}')(inputs[-1])
+        for i in range(self.n_scales - 1, -1, -1):
+            if i < self.n_scales - 1:
+                x = self.__getattr__(f'lateral_block_{i}')(inputs[i]) + x
+            outs.append(x)
+            if i > 0:
+                x = self.__getattr__(f'up_block_{i}')(x)
+        return outs[::-1]
+
+    def init_weights(self):
+        pass
+
+    @staticmethod
+    def _make_up_block(n_channels):
+        return nn.Sequential(
+            ME.MinkowskiGenerativeConvolutionTranspose(
+                n_channels,
+                n_channels,
+                kernel_size=2,
+                stride=2,
+                dimension=3,
+            ),
+            ME.MinkowskiBatchNorm(n_channels),
+            ME.MinkowskiReLU(),
+            ME.MinkowskiConvolution(n_channels, n_channels, kernel_size=3, dimension=3),
+            ME.MinkowskiBatchNorm(n_channels),
+            ME.MinkowskiReLU()
+        )
+
+    @staticmethod
+    def _make_block(in_channels, out_channels):
+        return nn.Sequential(
+            ME.MinkowskiConvolution(in_channels, out_channels, kernel_size=1, dimension=3),
+            ME.MinkowskiBatchNorm(out_channels),
+            ME.MinkowskiReLU()
+        )
