0cdd828647e75b4cc882dceed57552506224000a
Danila Rukhovich <danrukh@gmail.com>
2021-09-29 20:29:38 +0300

    rename configs

--- configs/fcaf3d/fcaf3d_sunrgbd-3d-10class.py
+++ configs/fcaf3d/fcaf3d_sunrgbd-3d-10class.py
@@ -9,32 +9,30 @@ model = dict(
         in_channels=3,
         depth=34),
     neck_with_head=dict(
-        type='ScanNetSparseFcos3DNeckWithHead',
+        type='SunRgbdSparseFcos3DNeckWithHead',
         in_channels=(64, 128, 256, 512),
         out_channels=128,
         pts_threshold=n_points,
-        n_classes=5,
+        n_classes=10,
         n_convs=0,
-        n_reg_outs=6,
+        n_reg_outs=8,
         voxel_size=voxel_size,
+        loss_bbox=dict(type='IoU3DLoss', loss_weight=1.0),
         assigner=dict(
-            type='ScanNetLimitedAssigner',
-            limit=27,
+            type='SunRgbdLimitedAssigner',
             topk=18,
+            limit=27,
             n_scales=4)),
-
     train_cfg=dict(),
     test_cfg=dict(
         nms_pre=1000,
         iou_thr=.5,
-        score_thr=.005))
+        score_thr=.01))
 
-dataset_type = 'S3DISDataset'
-data_root = './data/s3dis/'
-class_names = ('table', 'chair', 'sofa', 'bookcase', 'board')
-train_area = [1, 2, 3, 4, 6]
-test_area = 5
-
+dataset_type = 'SUNRGBDDataset'
+data_root = 'data/sunrgbd/'
+class_names = ('bed', 'table', 'sofa', 'chair', 'toilet', 'desk', 'dresser',
+               'night_stand', 'bookshelf', 'bathtub')
 train_pipeline = [
     dict(
         type='LoadPointsFromFile',
@@ -47,12 +45,11 @@ train_pipeline = [
     dict(
         type='RandomFlip3D',
         sync_2d=False,
-        flip_ratio_bev_horizontal=0.5,
-        flip_ratio_bev_vertical=0.5),
+        flip_ratio_bev_horizontal=0.5),
     dict(
         type='GlobalRotScaleTrans',
-        rot_range=[-0.087266, 0.087266],
-        scale_ratio_range=[.9, 1.1],
+        rot_range=[-0.523599, 0.523599],
+        scale_ratio_range=[0.85, 1.15],
         translation_std=[.1, .1, .1],
         shift_height=False),
     dict(type='DefaultFormatBundle3D', class_names=class_names),
@@ -96,24 +93,19 @@ data = dict(
     workers_per_gpu=4,
     train=dict(
         type='RepeatDataset',
-        times=10,
+        times=3,
         dataset=dict(
-            type='ConcatDataset',
-            datasets=[
-                dict(
-                    type=dataset_type,
-                    data_root=data_root,
-                    ann_file=data_root + f's3dis_infos_Area_{i}.pkl',
-                    pipeline=train_pipeline,
-                    filter_empty_gt=True,
-                    classes=class_names,
-                    box_type_3d='Depth') for i in train_area
-            ],
-            separate_eval=False)),
+            type=dataset_type,
+            data_root=data_root,
+            ann_file=data_root + 'sunrgbd_infos_train.pkl',
+            pipeline=train_pipeline,
+            filter_empty_gt=True,
+            classes=class_names,
+            box_type_3d='Depth')),
     val=dict(
         type=dataset_type,
         data_root=data_root,
-        ann_file=data_root + f's3dis_infos_Area_{test_area}.pkl',
+        ann_file=data_root + 'sunrgbd_infos_val.pkl',
         pipeline=test_pipeline,
         classes=class_names,
         test_mode=True,
@@ -121,7 +113,7 @@ data = dict(
     test=dict(
         type=dataset_type,
         data_root=data_root,
-        ann_file=data_root + f's3dis_infos_Area_{test_area}.pkl',
+        ann_file=data_root + 'sunrgbd_infos_val.pkl',
         pipeline=test_pipeline,
         classes=class_names,
         test_mode=True,
